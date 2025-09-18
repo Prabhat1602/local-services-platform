578e6e7e563c08a79e64079c7731b816e1d0a0ed
Prabhat Sahu <prabhatsahu@Prabhats-MacBook-Air.local>
2025-09-18 09:37:03 +0530

    fix: navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Notifications from './Notifications';
 const Navbar = () => {
+   console.log("Navbar component is rendering!"); // <--- ADD THIS
   const navigate = useNavigate();
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
